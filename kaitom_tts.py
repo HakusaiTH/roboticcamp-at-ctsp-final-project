2f6b197f5d4232800f1431cc27ab1c6d0cab8932
Fujiwara Aoyama <104154862+HakusaiTH@users.noreply.github.com>
2023-11-20 17:40:23 +0700

    Add files via upload

---
+++ kaitom_tts.py
@@ -0,0 +1,35 @@
+import iapp_ai
+import pygame
+
+def generate_and_play_audio(text):
+    # You can request an API key at https://ai.iapp.co.th
+    apikey = 'v076bYzziXnBKo0BvxvA65xjzrRTQMIY'
+
+    api = iapp_ai.api(apikey)
+
+    output_file = "output.mp3"
+    response = api.thai_thaitts_kaitom(text)
+
+    with open(output_file, "wb") as f:
+        f.write(response.content)
+
+    # Initialize pygame
+    pygame.init()
+    pygame.mixer.init()
+
+    # Load and play the audio
+    pygame.mixer.music.load(output_file)
+
+    pygame.mixer.music.play()
+
+    # Wait for the audio to finish playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    # Clean up pygame
+    pygame.quit()
+
+'''
+user_input = input("x")
+generate_and_play_audio(user_input)
+'''
